d7bb099fd6bbd6f7fc6ee2dcd42950b02f5496e4
Savchenko <s.savchenko@roseltorg.ru>
2018-08-06 15:44:41 +0300

    learn 3

---
+++ skeleton/src/Migrations/Version20180802075832.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180802075832 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE article CHANGE publishe_at published_at DATETIME DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_23A0E66FEC530A9 ON article (content)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_23A0E66FEC530A9 ON article');
+        $this->addSql('ALTER TABLE article CHANGE published_at publishe_at DATETIME DEFAULT NULL');
+    }
+}
